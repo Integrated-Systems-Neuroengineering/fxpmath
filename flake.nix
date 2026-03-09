3bda26f7a6a8f6e5af6e0e5169bfe1143f008da5
Gwenevere Frank <gwen@crisdsc0.ucsd.edu>
2026-03-09 12:11:20 -0700

    pin nixpkgs to 24.11, preferWheels

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "fxpmath - Fractional fixed-point arithmetic with Numpy compatibility";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
     devshell = {
       url = "github:numtide/devshell";
@@ -23,6 +23,7 @@
         packages.default = p2n.mkPoetryApplication {
           projectDir = ./.;
           python = pkgs.python311;
+          preferWheels = true;
         };
 
         devShells.default = pkgs.devshell.mkShell {
